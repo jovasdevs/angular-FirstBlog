271e8584ce16f639d10ebe1f5f380891e4337aa3
jovasdevs <jovasdevs@gmail.com>
2022-12-27 20:26:43 -0300

    components createds with styles

---
+++ src/app/components/menu-title/menu-title.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-menu-titile',
+  templateUrl: './menu-title.component.html',
+  styleUrls: ['./menu-title.component.css']
+})
+export class MenuTitileComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}

a0bb2351f6621c070b1d36fad386fa720f0c738b
jovasdevs <jovasdevs@gmail.com>
2022-12-29 09:22:39 -0300

    content created

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,16 @@
+export const dataFake = [
+  {
+    "id": "1",
+    "photo": "image",
+    "title": "titulo",
+    "description": "JavaScript é uma linguagem de programação interpretada estruturada, de script em alto nível com tipagem dinâmica fraca e multiparadigma. Juntamente com HTML e CSS, o JavaScript é uma das três principais tecnologias da World Wide Web.",
+    "photoCover": "https://miro.medium.com/max/720/1*YPSX0nSww0SsNGCQyBNOpA.webp",
+  },
+  {
+    "id": "2",
+    "photo": "...",
+    "title": "...",
+    "description": "...",
+    "photoCover": ""
+  }
+]
